668d0b7b4d9ab7acc2cdb001d74bc46adc09d7df
irerin07 <counterchord89@gmail.com>
2023-10-01 22:55:41 +0900

    Initial commit

---
+++ src/test/java/com/tistory/irerin07/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.irerin07.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
